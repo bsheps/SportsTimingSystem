cba62ea90a917e7dd2e5ed3b48ebb1508d145071
BS <bshepard@uwm.edu>
2018-03-09 20:33:41 -0600

    Alphabetized.

--- src/Commands.java
+++ src/Commands.java
@@ -1,23 +1,23 @@
 import java.time.LocalTime;
 
 public interface Commands {
-void POWER();
-void EXIT();
-void RESETn();
-void TIMEset(LocalTime time);
-void TOG(int channelNumber);
-void CONN(String sensorType, int channel); // connect a sensor
-void DISC(int channel2disconnect);
-void EVENT(String eventName);
-void NEWRUN();
-void ENDRUN();
-void PRINT(int runNumber); // 
-void EXPORT(int runNumber); // 
-void NUM(int bibNumber); // set next competitor to start
-void CLR(int bibNumber); // remove the competitor from queue
-void SWAP(); // swap the next 2 competitor from queue
-void DNF();	// next racer in queue will not finish
-void TRIG(int channelNumber);
-void START();	// trigger channel 1
-void FINISH();	// trigger channel 2
+	void CLR(int bibNumber); // remove the competitor from queue
+	void CONN(String sensorType, int channel); // connect a sensor
+	void DISC(int channel2disconnect);
+	void DNF();	// next racer in queue will not finish
+	void ENDRUN();
+	void EVENT(String eventName);
+	void EXIT();
+	void EXPORT(int runNumber); // 
+	void FINISH();	// trigger channel 2
+	void NEWRUN();	
+	void NUM(int bibNumber); // set next competitor to start
+	void POWER();
+	void PRINT(int runNumber); // 
+	void RESET();
+	void TIMEset(LocalTime time);
+	void TOG(int channelNumber);
+	void TRIG(int channelNumber);	
+	void START();	// trigger channel 1
+	void SWAP(); // swap the next 2 competitor from queue	
 }
