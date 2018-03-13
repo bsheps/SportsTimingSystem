bc5fada5726aacc972673a6e786dc0d48434d942
bjf <bjf@10.21.29.238>
2018-03-13 18:48:00 -0500

    added a getResults method...

--- src/EventInterface.java
+++ src/EventInterface.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.Queue;
 
 public interface EventInterface {
 	void addRacer(String bibNumber);
 	void trigger(int channelNumber);
 	Queue<Racer> moveAll();
-	Queue<Racer> getFinishers();
+	ArrayList<Racer> getResults(); //return (finished + racing + queued)
 }
