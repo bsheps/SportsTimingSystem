a0f879d8d22bf5a3958afcc1c8f9d83b0d367b4e
bjf <bjf@10.21.29.238>
2018-03-13 18:52:54 -0500

    Reduced/optimized much code. Made EXPORT and PRINT work. Commented lots.

--- src/ChronoTimer.java
+++ src/ChronoTimer.java
@@ -1,6 +1,11 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Queue;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * This class handles the administrative parts of instantiating a race. It
@@ -9,35 +14,53 @@ import java.util.Queue;
  * events.
  * 
  * @author BS
+ * @author bjf
  *
  */
 public class ChronoTimer implements CommandsInterface {
+	/*
+	 * I made all data fields here private to ensure they are not accessed from the
+	 * outside without using our getters and setters.
+	 */
 
-	Channel chan;
-	String _eventName;
-	// EventInterface _event;
-	ParaIndEvent parind;
-	IndividualEvent ind;
-	boolean _powerOn;
-	boolean _raceInSession;
-	Printer print;
-	boolean parIndEvent, indiEvent;
-	ArrayList<Queue<Racer>> storageUnit;
-	ArrayList<EventInterface> oldRaces;
+	// bjf: chan (Channel) will be set when POWER() is called.
+	private Channel chan;
 
+	// bjf: print is initialized in constructor
+	private Printer print;
+
+	// bjf: initialize EventInterface with NEWRUN() after EVENT() has been called
+	private EventInterface _event;
+
+	// Title of current event name
+	private String _eventName;
+
+	// Keep track of CT state
+	private boolean _powerOn, _raceInSession;
+
+	// bjf: ?
+	private ArrayList<Queue<Racer>> storageUnit;
+
+	// bjf: finishedRuns is holding all of the old events, no matter the type
+	private ArrayList<EventInterface> finishedRuns;
+
+	/**
+	 * bjf: This constructor initializes the standard components and state
+	 * variables. I removed the booleans, "ind" and "parind" because I believe they
+	 * store redundant information. I am opting to store this information as a
+	 * string in the _eventName field.
+	 * 
+	 * @throws IOException
+	 *             (See Printer() documentation)
+	 */
 	public ChronoTimer() throws IOException {
-		this.chan = new Channel();
-		this._eventName = null; // could set default to "IND"
-		// this._event = null; //redundant
-		parind = new ParaIndEvent();
-		ind = new IndividualEvent();
 		this._powerOn = false;
 		this._raceInSession = false;
+		// this._eventName = null; //redundant
+		// this._event = null; //redundant
 		this.print = new Printer();
-		this.parIndEvent = false;
-		this.indiEvent = false;
 		this.storageUnit = new ArrayList<Queue<Racer>>();
-		oldRaces = new ArrayList<EventInterface>();
+		this.finishedRuns = new ArrayList<EventInterface>();
 	}
 
 	// public void printResults() {
@@ -69,36 +92,49 @@ public class ChronoTimer implements CommandsInterface {
 	// // }
 	// }
 	// }
-	// clears the memory
+
+	/**
+	 * clears the memory
+	 * 
+	 * @throws IOException
+	 */
 	public void reset() throws IOException {
 		print.PrinterRest();
 	}
 
+	/**
+	 * Send a string to the printer from outside this class.
+	 * 
+	 * @param string
+	 *            - text to be printed.
+	 */
 	public void usePrinter(String string) {
 		print.printThis(string);
 	}
 
 	public void endRun() {
 		_raceInSession = false;
+		storageUnit.add(_event.moveAll());
 
-		if (_eventName.equals("IND")) {
-			storageUnit.add(ind.moveAll());
-		} else if (_eventName.equals("PARIND")) {
-			storageUnit.add(parind.moveAll());
-		}
 		/**
+		 * For Future Use
+		 * 
 		 * else if (_eventName.equals("GRP")) { storageUnit.add(groupEvent.moveAll()); }
 		 * else { storageUnit.add(paraGroupEvent.moveAll()); }
 		 * 
-		 * For Future Use
 		 **/
 
 		print.printThis("Ending current run");
 
 	}
 
-	public int getOldRacesSize() {
-		return oldRaces.size();
+	/**
+	 * bjf: simple field getter
+	 * 
+	 * @return int - the size of the numbered runs
+	 */
+	public int getFinishedRunsSize() {
+		return finishedRuns.size();
 	}
 
 	@Override
@@ -127,6 +163,10 @@ public class ChronoTimer implements CommandsInterface {
 	@Override
 	public void ENDRUN() {
 		_raceInSession = false;
+		finishedRuns.add(_event);
+		if (_event != null) {
+			storageUnit.add(_event.moveAll());
+		}
 		// TODO Storage of the results needs to happen here
 
 	}
@@ -148,9 +188,43 @@ public class ChronoTimer implements CommandsInterface {
 
 	}
 
+	/*
+	 * bjf: should not be able to export a race in progress
+	 * 
+	 * @see CommandsInterface#EXPORT(int)
+	 */
 	@Override
 	public void EXPORT(int runNumber) {
+		if (runNumber <= 0 || runNumber > finishedRuns.size()) {
+			throw new IllegalArgumentException("Could not find run: " + runNumber);
+		}
+		// _event.moveAll();
+		ArrayList<Racer> thisRun = finishedRuns.get(runNumber).getResults();
+		if (thisRun.size() == 0) {
+			System.out.println("   JSON EXPORT FOUND NO CONTENT");
+		} else {
 
+			/*
+			 * bjf: The following three lines are somewhat confusing, but necessary to
+			 * prevent a JSON error regarding illegal reflective access... basically I am
+			 * using an adapter to define how JSON should parse a racer class. This way it
+			 * will not serialize the data
+			 */
+			GsonBuilder builder = new GsonBuilder();
+			builder.registerTypeAdapter(Racer.class, new RacerAdapter());
+			Gson g = builder.create();
+			/* End confusing bit */
+			
+			String out = g.toJson(thisRun);
+			File f = new File("RUN" + runNumber + ".txt");
+
+			try (PrintWriter pw = new PrintWriter(f);) {
+				pw.write(out.toString());
+				System.out.println("   JSON EXPORT SUCCESSFUL");
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
@@ -161,72 +235,58 @@ public class ChronoTimer implements CommandsInterface {
 
 	@Override
 	public void NEWRUN() {
-		if (_raceInSession)
+		if (_raceInSession) {
 			System.out.println("ERROR RACE IN SESSION: End current run before starting a NEWRUN.");
-		else {
+		} else {
 			_raceInSession = true;
 
-			/*
-			 * OLD CODE // bjf: This is a problem. If we overwite our events, //then we lose
-			 * the old event data. // if (_eventName.equals("IND")) { // ind = new
-			 * IndividualEvent(); // } else if (_eventName.equals("PARIND")) { // parind =
-			 * new ParaIndEvent(); // }
-			 */
-
-			/* NEW CODE // this way we save the */
 			if (_eventName.equals("IND")) {
-				ind.moveAll();
-				oldRaces.add(ind);
-				ind = new IndividualEvent();
+				_event = new IndividualEvent();
 			} else if (_eventName.equals("PARIND")) {
-				parind.moveAll();
-				oldRaces.add(parind);
-				parind = new ParaIndEvent();
+				_event = new ParaIndEvent();
 			}
 
 		}
 	}
 
 	public void NUM(String bibNumber) {
-		if (indiEvent)
-			ind.addRacer(bibNumber);
-		else if (parIndEvent)
-			parind.addRacer(bibNumber);
+		_event.addRacer(bibNumber);
 	}
 
 	public void POWER() {
 		if (!_powerOn) {
-			_powerOn = true;
 			chan = new Channel();
-			_raceInSession = false;
 			Time.startTime();
 		} else if (_powerOn) {
-			_powerOn = false;
-			_raceInSession = false;
-			if (_eventName.equals("IND") && ind != null) {
-				print.printThis("POWERING OFF- PENDING ITEMS:");
-				// printResults();
-				print.shutDownPrinter();
-			}
-
-			if (_eventName.equals("PARIND") && parind != null) {
-				print.printThis("POWERING OFF - PENDING ITEMS:");
-				// printResults();
-				print.shutDownPrinter();
-			}
-
+			print.printThis("POWERING OFF- PENDING ITEMS:");
+			print.shutDownPrinter();
 		}
+		_powerOn = !_powerOn;
+		_raceInSession = false;
+
 	}
 
+	/*
+	 * bjf: wasn't sure exactly what they were looking for with this command,
+	 * because I didn't see an entry in the Sprint 1 or Sprint 2 pdf's about this,
+	 * but it was in the sprint2test.txt that was given. I decided that because it
+	 * was only called at the end of a run, that it should just print the results of
+	 * the last/current race. I didn't see any "PRINT ##" commands in the test
+	 * files, and that is why I removed the runNumber argument from the method
+	 * header.
+	 * 
+	 * (non-Javadoc)
+	 * 
+	 * @see CommandsInterface#PRINT(int)
+	 */
 	@Override
-	public void PRINT(int runNumber) {
-		if(runNumber < 0 || runNumber > oldRaces.size()) {
-			throw new IllegalArgumentException("Cannot find this run number.");
+	public void PRINT() {
+		ArrayList<Racer> results = _event.getResults();
+
+		for (Racer r : results) {
+			print.printThis("Bib Number = " + r.getBibNum() + ", Result = " + r.results());
 		}
-		 Queue<Racer> loadResult = oldRaces.get(runNumber).getFinishers();
-		 for (Racer r : loadResult) {
-		 print.printThis(r.getBibNum() + " :: " + r.results());
-		 }
+
 	}
 
 	@Override
@@ -238,38 +298,36 @@ public class ChronoTimer implements CommandsInterface {
 	@Override
 	public void TIME(String time) {
 		Time.setTime(time);
-
 	}
 
 	@Override
 	public void TOG(int channelNumber) {
-		if (chan.Toggle(channelNumber))
+		if (chan.Toggle(channelNumber)) {
 			System.out.println("Channel " + channelNumber + " was enabled.");
-		else
+		} else {
 			System.out.println("Channel " + channelNumber + " was disabled.");
+		}
 	}
 
 	// could we pass on the event type because this is only for indrun
 	@Override
 	public void TRIG(int channelNumber) {
+		if (!_raceInSession) {
+			throw new RuntimeException("Could not trigger channel " + channelNumber + ". Race not is session.");
+		}
 		if (chan.isChannelEnabled(channelNumber) && _raceInSession) {
-			if (_eventName.equals("IND"))
-				ind.trigger(channelNumber);
-			else if (_eventName.equals("PARIND"))
-				parind.trigger(channelNumber);
-
+			_event.trigger(channelNumber);
 		}
 		// else do nothing, channel is disabled or a race is not in session
 	}
 
 	@Override
 	public void START() {
-		if (_eventName.equals("IND")) {
-			ind.trigger(1);
-		} else if (_eventName.equals("PARIND")) {
-			parind.trigger(1);
-		}
-
+		/*
+		 * I simplified this method for now. There should be an easier way to access
+		 * start triggers than by hard-coding them in.
+		 */
+		_event.trigger(1);
 	}
 
 	@Override
