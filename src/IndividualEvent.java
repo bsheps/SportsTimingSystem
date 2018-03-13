553335d9ff06944e05a8ea7e24ce61c83ac08324
bjf <bjf@10.21.29.238>
2018-03-13 18:48:57 -0500

    implemented getResults, and added a comment or two

--- src/IndividualEvent.java
+++ src/IndividualEvent.java
@@ -1,49 +1,57 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class IndividualEvent implements EventInterface{
+public class IndividualEvent implements EventInterface {
 	Queue<Racer> WaitingToRace = (Queue<Racer>) new LinkedList<Racer>(),
-			inTheRace = (Queue<Racer>) new LinkedList<Racer>(),
-			finishers = (Queue<Racer>) new LinkedList<Racer>();
+			inTheRace = (Queue<Racer>) new LinkedList<Racer>(), finishers = (Queue<Racer>) new LinkedList<Racer>();
 
-	public void addRacer(String name){
+	public void addRacer(String name) {
 		WaitingToRace.add(new Racer(name));
 	}
+
 	/**
-	 * Indicates that a sensor was "triggered" and stores the corresponding time in 
-	 * either the start or finish fields of the racer (odd channels start, even finish)
+	 * Indicates that a sensor was "triggered" and stores the corresponding time in
+	 * either the start or finish fields of the racer (odd channels start, even
+	 * finish)
+	 * 
 	 * @param chNum
 	 * @param Time.getCurrentTime()
 	 */
 	public void trigger(int chNum) {
-		if(chNum   == 1){	// odd means start time
-			if(WaitingToRace.size() == 0) {
+		if (chNum == 1) { // odd means start time
+			if (WaitingToRace.size() == 0) {
 				Racer n = new Racer("noName");
 				n.startRace(Time.getCurrentTime());
 				inTheRace.add(n);
-				//print.printThis(" Channel " + chNum+ " triggered at " +Time.time2formattedString(Time.getCurrentTime()) + " for racer: noName");
-			}
-			else {
+				// print.printThis(" Channel " + chNum+ " triggered at "
+				// +Time.time2formattedString(Time.getCurrentTime()) + " for racer: noName");
+			} else {
 				Racer x = WaitingToRace.remove();
 				x.startRace(Time.getCurrentTime());
-				//print.printThis(" Channel " + chNum+ " triggered at " +Time.time2formattedString(Time.getCurrentTime()) + " for racer: " +x._bibNum);
+				// print.printThis(" Channel " + chNum+ " triggered at "
+				// +Time.time2formattedString(Time.getCurrentTime()) + " for racer: "
+				// +x._bibNum);
 				inTheRace.add(x);
 				x = null;
 			}
-		}
-		else if(chNum == 2){	// even means end race
-			if(inTheRace.size() == 0); //print.printThis("Channel " + chNum +" triggered at " + Time.time2formattedString(Time.getCurrentTime()) +"--ERROR: No more racers");
+		} else if (chNum == 2) { // even means end race
+			if (inTheRace.size() == 0)
+				; // print.printThis("Channel " + chNum +" triggered at " +
+					// Time.time2formattedString(Time.getCurrentTime()) +"--ERROR: No more racers");
 			else {
 				Racer y = inTheRace.remove();
 				y.finishRace(Time.getCurrentTime());
-				//print.printThis(" Channel " + chNum+ " triggered at " +Time.time2formattedString(Time.getCurrentTime()) + " for racer: " +y._bibNum);
+				// print.printThis(" Channel " + chNum+ " triggered at "
+				// +Time.time2formattedString(Time.getCurrentTime()) + " for racer: "
+				// +y._bibNum);
 				finishers.add(y);
 			}
 		}
 	}
-	
-	public Queue<Racer> moveAll(){
-		for (Racer x : inTheRace ) {
+
+	public Queue<Racer> moveAll() {
+		for (Racer x : inTheRace) {
 			x = inTheRace.remove();
 			finishers.add(x);
 		}
@@ -51,12 +59,18 @@ public class IndividualEvent implements EventInterface{
 			y = WaitingToRace.remove();
 			finishers.add(y);
 		}
-		
+
 		return finishers;
 	}
+
 	@Override
-	public Queue<Racer> getFinishers() {
-		// TODO Auto-generated method stub
-		return finishers;
+	public ArrayList<Racer> getResults() {
+		ArrayList<Racer> results = new ArrayList<Racer>();
+
+		results.addAll(this.finishers);
+		results.addAll(this.inTheRace);
+		results.addAll(this.WaitingToRace);
+
+		return results;
 	}
 }
--- src/ParaIndEvent.java
+++ src/ParaIndEvent.java
@@ -1,17 +1,16 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * @author faassad
- * channel 1,3 is start, channel 2,4 is for finish 
+ * @author faassad channel 1,3 is start, channel 2,4 is for finish
  */
 
-public class ParaIndEvent implements EventInterface{
-	Queue<Racer> channels12= (Queue<Racer>) new LinkedList<Racer>(),
-			finishers= (Queue<Racer>) new LinkedList<Racer>(),
+public class ParaIndEvent implements EventInterface {
+	Queue<Racer> channels12 = (Queue<Racer>) new LinkedList<Racer>(),
+			finishers = (Queue<Racer>) new LinkedList<Racer>(),
 			channels34 = (Queue<Racer>) new LinkedList<Racer>(),
-			waitingToRace = (Queue<Racer>) new LinkedList<Racer>(); 
-
+			waitingToRace = (Queue<Racer>) new LinkedList<Racer>();
 
 	public void addRacer(String name) {
 		waitingToRace.add(new Racer(name));
@@ -21,52 +20,50 @@ public class ParaIndEvent implements EventInterface{
 	 * @param channelNumber
 	 * @param Time.getCurrentTime()
 	 */
-	public void trigger(int channelNumber) {		
-			if(channelNumber==1) {
-				if(waitingToRace.size()==0) 
-					channels12.add(new Racer("noName", Time.getCurrentTime()));
-				else {
-					Racer racer = waitingToRace.remove();
-					racer.startRace(Time.getCurrentTime());
-					channels12.add(racer);
-				}
+	public void trigger(int channelNumber) {
+		if (channelNumber == 1) {
+			if (waitingToRace.size() == 0)
+				channels12.add(new Racer("noName", Time.getCurrentTime()));
+			else {
+				Racer racer = waitingToRace.remove();
+				racer.startRace(Time.getCurrentTime());
+				channels12.add(racer);
 			}
-			else if(channelNumber==2) {
-				Racer racer = channels12.remove();
-				racer.finishRace(Time.getCurrentTime());
-				finishers.add(racer);
+		} else if (channelNumber == 2) {
+			Racer racer = channels12.remove();
+			racer.finishRace(Time.getCurrentTime());
+			finishers.add(racer);
+		} else if (channelNumber == 3) {
+			if (waitingToRace.size() == 0)
+				channels34.add(new Racer("noName", Time.getCurrentTime()));
+			else {
+				Racer racer = waitingToRace.remove();
+				racer.startRace(Time.getCurrentTime());
+				channels34.add(racer);
 			}
-			else if(channelNumber==3) {
-				if(waitingToRace.size()==0) 
-					channels34.add(new Racer("noName", Time.getCurrentTime()));
-				else {
-					Racer racer = waitingToRace.remove();
-					racer.startRace(Time.getCurrentTime());
-					channels34.add(racer);
-				}
-			}
-			else if(channelNumber==4) {
-				Racer racer = channels34.remove();
-				racer.finishRace(Time.getCurrentTime());
-				finishers.add(racer);
-			}
+		} else if (channelNumber == 4) {
+			Racer racer = channels34.remove();
+			racer.finishRace(Time.getCurrentTime());
+			finishers.add(racer);
+		}
 	}
 
 	public void endEvent(boolean endRace) {
-		if(endRace) {
-			while(channels12.size()!=0)
+		// bjf: I think we should set the finish time to null for unfinished racers...
+		if (endRace) {
+			while (channels12.size() != 0)
 				finishers.add(channels12.remove());
-			while(channels34.size()!=0)
+			while (channels34.size() != 0)
 				finishers.add(channels34.remove());
 		}
 	}
-	
+
 	public Queue<Racer> moveAll() {
-		for (Racer x : channels12 ) {
+		for (Racer x : channels12) {
 			x = channels12.remove();
 			finishers.add(x);
 		}
-		for (Racer y : channels34 ) {
+		for (Racer y : channels34) {
 			y = channels34.remove();
 			finishers.add(y);
 		}
@@ -78,9 +75,15 @@ public class ParaIndEvent implements EventInterface{
 	}
 
 	@Override
-	public Queue<Racer> getFinishers() {
-		// TODO Auto-generated method stub
-		return finishers;
+	public ArrayList<Racer> getResults() {
+		ArrayList<Racer> results = new ArrayList<Racer>();
+		
+		 results.addAll(this.finishers);
+		 results.addAll(this.channels12);
+		 results.addAll(this.channels34);
+		 results.addAll(this.waitingToRace);
+
+		return results;
 	}
 
 }
