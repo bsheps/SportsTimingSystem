9645ce6ffa1ca4fa2c63ee8c15425f4f286e8e0c
bjf <bjf@10.21.12.59>
2018-03-13 14:20:08 -0500

    added getFinishers()

--- src/EventInterface.java
+++ src/EventInterface.java
@@ -1,7 +1,8 @@
 import java.util.Queue;
 
 public interface EventInterface {
-void addRacer(String bibNumber);
-void trigger(int channelNumber);
-Queue<Racer> moveAll();
+	void addRacer(String bibNumber);
+	void trigger(int channelNumber);
+	Queue<Racer> moveAll();
+	Queue<Racer> getFinishers();
 }
