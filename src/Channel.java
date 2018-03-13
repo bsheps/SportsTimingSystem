67a4b39354d8eb0991fa28f2b8edc7eed04aaedd
bjf <bjf@10.21.29.238>
2018-03-13 18:44:13 -0500

    simply added a comment description

--- src/Channel.java
+++ src/Channel.java
@@ -1,22 +1,32 @@
 
-public class Channel{
+public class Channel {
 	private boolean[] channel = new boolean[12];
 	private String[] sensor = new String[12];
+
 	public Channel() {
-		for(int i = 0; i < channel.length; i++) 
+		for (int i = 0; i < channel.length; i++)
 			channel[i] = false;
 	}
+
+	/**
+	 * @param ch
+	 *            - Channel number to toggle
+	 * @return boolean - new channel state
+	 */
 	public boolean Toggle(int ch) {
 		channel[ch] = !channel[ch];
-		//print.printThis("Channel "+ch+" is " + (channel[ch]? "Enabled": "Disabled"));
+		// print.printThis("Channel "+ch+" is " + (channel[ch]? "Enabled": "Disabled"));
 		return channel[ch];
 	}
+
 	public boolean isChannelEnabled(int channelNumber) {
 		return channel[channelNumber];
 	}
+
 	public boolean connectSensor(String sensortype, int chan) {
-		if(sensor[chan] != null) {return false;/*can't connect because something is already there*/}
-		else {
+		if (sensor[chan] != null) {
+			return false;
+			/* can't connect because something is already there */} else {
 			sensor[chan] = sensortype;
 			return true;
 		}
