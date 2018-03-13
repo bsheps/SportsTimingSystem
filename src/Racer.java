a6bf46b7059bddd944426f673c7bc8472eb23095
bjf <bjf@10.21.29.238>
2018-03-13 13:08:46 -0500

    Linked constructors, added another constructor. Added getter for bibNum

--- src/Racer.java
+++ src/Racer.java
@@ -5,44 +5,62 @@ import java.time.format.DateTimeFormatter;
  * @author bshepard Racers has 3 fields: name, startTime and endTime
  */
 public class Racer {
-	final DateTimeFormatter formatTime=DateTimeFormatter.ofPattern("HH:mm:ss.SS");;
+	final DateTimeFormatter formatTime = DateTimeFormatter.ofPattern("HH:mm:ss.SS");;
 	String _bibNum;
 	LocalTime _startTime, _endTime;
+
 	/**
 	 * Constructs a racer
+	 * 
 	 * @param name
 	 */
-	public Racer(String name){
-		_startTime = _endTime = null;
-		_bibNum = name;
+	public Racer(String name) {
+		this(name, null, null);
 	}
+
 	public Racer(String name, LocalTime start) {
-		_startTime = start;
-		_endTime = null;
-		_bibNum = name;
+		this(name, start, null);
 	}
+
+	public Racer(String name, LocalTime start, LocalTime end) {
+		if (name == null) {
+			throw new IllegalArgumentException("Racer should not have a null name/bibNum");
+		}
+		_bibNum = name;
+		_startTime = start;
+		_endTime = end;
+	}
+
+	public String getBibNum() {
+		return _bibNum;
+	}
+
 	/**
 	 * Sets the start time for racer
+	 * 
 	 * @param time
 	 */
 	public void startRace(LocalTime time) {
 		_startTime = time;
 	}
+
 	/**
 	 * Sets the end time for racer
+	 * 
 	 * @param time
 	 */
 	public void finishRace(LocalTime time) {
 		_endTime = time;
 	}
+
 	/**
 	 * 
 	 * @return a printer friendly string of the racer's time
 	 */
 	public String results() {
-		if(_endTime == null || _startTime == null)return "Error printing results";
+		if (_endTime == null || _startTime == null)
+			return "Error printing results";
 		return LocalTime.ofNanoOfDay(_endTime.toNanoOfDay() - _startTime.toNanoOfDay()).format(formatTime);
 	}
-
 
 }
